5252d31688a2a1c9a42d35153fb0b2d7f20d1840
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2022-12-30 08:35:32 +0200

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,7 +2,6 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEl = document.querySelector(".gallery");
 
-
 const itemsEl = galleryItems.map(({ preview, original, description }) => {
     return `<a class="gallery__item" href="${original}">
                 <img class="gallery__image" src="${preview}" data-large="${original}" alt="${description}" />
